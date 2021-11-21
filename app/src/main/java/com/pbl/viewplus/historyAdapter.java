cac784bae2566eb9c9a2d68a7b17798c9cd91220
unknown <kawkjenny11@gmail.com>
2021-11-21 16:13:34 +0900

    design

--- app/src/main/java/com/pbl/viewplus/historyAdapter.java
+++ app/src/main/java/com/pbl/viewplus/historyAdapter.java
@@ -2,40 +2,55 @@ package com.pbl.viewplus;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class historyAdapter extends RecyclerView.Adapter<historyAdapter.ViewHolder>{
 
-    private ArrayList<hDataitem> hData = null ;
+    private ArrayList<hDataitem> hData = null;
     private Intent intent;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     //아이템 뷰 저장하는 뷰 홀더 클래스
     public class ViewHolder extends RecyclerView.ViewHolder{
-        LinearLayout hdata_linearLayout;
-        TextView text;
+        LinearLayout hDataLinearLayout;
+        TextView itemText;
+        Button delButton;
+
         ViewHolder(View itemView){
             super(itemView);
-            hdata_linearLayout=itemView.findViewById(R.id.hdata_linearLayout);
-            text=itemView.findViewById(R.id.itemtext);
+            hDataLinearLayout = itemView.findViewById(R.id.hdata_linearLayout);
+            itemText = itemView.findViewById(R.id.itemtext);
+            delButton = itemView.findViewById(R.id.delButton);
         }
     }
+
     // 생성자에서 데이터 리스트 객체를 전달받음.
     historyAdapter(ArrayList<hDataitem> list) {
         hData = list ;
     }
 
-    //onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴. viewType 형태의 아이템 뷰를 위한 뷰홀더 객체 생성.
+    // onCreateViewHolder() - ViewHolder 를 새로 만들어야 할 때 호출되는 메소드.
+    // 각 아이템을 위한 XML 레이아웃을 활용한 뷰 객체를 생성하고 이를 뷰 홀더 객체에 담아 리턴.
+    // ViewHolder 가 아직 어떠한 데이터에 바인딩된 상태가 아니기 때문에 각 뷰의 내용은 채우지 않음.
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context=parent.getContext();
+        Context context = parent.getContext();
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
 
         View view = inflater.inflate(R.layout.hdata_item, parent, false) ;
@@ -44,15 +59,15 @@ public class historyAdapter extends RecyclerView.Adapter<historyAdapter.ViewHold
         return vh;
     }
 
-    //position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
+    // position 에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         hDataitem item = hData.get(position) ;
+        String date = item.getDate();
+        holder.itemText.setText(item.getIv1());
 
-        String date= item.getDate();
-        holder.text.setText(item.getIv1());
-
-        holder.hdata_linearLayout.setOnClickListener(new View.OnClickListener(){
+        // item 의 text
+        holder.itemText.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 //버튼을 쓰레기로 바꿀거면 분석과정 안일어나게 하던지 아님 새로 액티비티 파던지. -간단하게 파자..
@@ -63,11 +78,30 @@ public class historyAdapter extends RecyclerView.Adapter<historyAdapter.ViewHold
             }
         });
 
-        //삭제 버튼 구현할때 위에코드 참고!!!!!!!!!!!!!!!!!!!!!!!!!!!(hdata_linearLayout)
+        // item 의 삭제 버튼
+        holder.delButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("ooo").document(date)
+                        .delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("hello", "DocumentSnapshot successfully deleted!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("hello", "Error deleting document", e);
+                            }
+                        });
 
+                hData.remove(position);
+                notifyItemRemoved(position);
+            }
+        });
     }
-
-
 
     @Override
     public int getItemCount() {
