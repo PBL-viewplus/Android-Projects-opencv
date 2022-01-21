6ad846e8f48c7bb2564811c8331922f327648b7e
Jiwoon2 <dkswjsgkek@naver.com>
2022-01-21 21:54:55 +0900

    0121 튜토리얼 tts 진행중
    +minsdk 26 수정

--- app/src/main/java/com/pbl/viewplus/Fragment1.java
+++ app/src/main/java/com/pbl/viewplus/Fragment1.java
@@ -8,57 +8,56 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment1 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment1() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment1 newInstance(String param1, String param2) {
-        Fragment1 fragment = new Fragment1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    View v;
+    TTS_controller tts = new TTS_controller();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+
+        v= inflater.inflate(R.layout.fragment_1, container, false);
+
+        //프래그먼트 이동했을때 바로 tts되야됨
+        // -> 처음 한번만 들려줌. 전환될때마다 다시 들려줘야됨.
+        // 프랙2까지 한번에 생성돼서 tts가 바로 이어져버림.
+        //생명주기?
+
+
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        String ex1="안녕하세요";
+
+        tts.initTTS(v.getContext(), ex1);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public void onStop(){
+        super.onStop();
+        tts.ttsStop();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (tts != null){
+            tts.ttsDestory();
+        }
     }
 }
